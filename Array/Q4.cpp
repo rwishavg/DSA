f6541b4c59f43adcfad95745b7d9420015e8a20b
rwishavg <grwishav34@gmail.com>
2021-07-24 18:58:07 +0530

    update

--- Array/Q4.cpp
+++ Array/Q4.cpp
@@ -25,31 +25,74 @@ typedef unsigned long long ull;
 
 using namespace std;
 
+/* O(2n) solution
 void solve(){
     int n;
     cin >> n;
     vector<int> v(n);
-    vector<int> ans;
     loop(i, n){
         cin >> v[i];
     }
+    vector<int> v2;
+    int a=0, b=0, c=0;
     loop(i, n){
-        if(v[i] == 0)
-          ans.pb(v[i]);
+        if(v[i] == 0){
+            a++;
+        }else if(v[i] == 1){
+            b++;
+        }else{
+            c++;
+        }
     }
+    loop(i, a)
+        v2.pb(0);
+    loop(i, b)
+        v2.pb(1);
+    loop(i, c)
+        v2.pb(2);
     loop(i, n){
-        if(v[i] == 1)
-          ans.pb(v[i]);
+        cout << v2[i] << " ";
     }
-    loop(i, n){
-        if(v[i] == 2)
-          ans.pb(v[i]);
-    }
-    loop(i, n){
-        cout << ans[i] << " ";
-    }
-}           
+    cout << endl;
+}
+*/
 
+//O(n) solution
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    loop(i, n){
+        cin >> a[i];
+    }
+    int lo = 0;
+    int hi = n - 1;
+    int mid = 0;
+ 
+    // Iterate till all the elements
+    // are sorted
+    while (mid <= hi) {
+        switch (a[mid]) {
+ 
+        // If the element is 0
+        case 0:
+            swap(a[lo++], a[mid++]);
+            break;
+ 
+        // If the element is 1 .
+        case 1:
+            mid++;
+            break;
+ 
+        // If the element is 2
+        case 2:
+            swap(a[mid], a[hi--]);
+            break;
+        }
+    }
+    loop(i,n)
+        cout<<a[i]<<" ";
+}
 
 int main() {
     IOS;
