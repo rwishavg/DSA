1ba2e6082e642bbf362e9a8249e6d4276d9a0f94
rwishavg <grwishav34@gmail.com>
2021-11-14 12:30:25 +0530

    refresh

---
+++ Practice/test.cpp
@@ -0,0 +1,38 @@
+//Question:
+
+//Code:
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef unsigned long long ull;
+#define whatis(x) cout << #x << " = " << x << endl;
+#define whatis2(x, y) cout << #x << " = " << x << " and " << #y << " = " << y << endl;
+#define MOD 1000000007
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define testcase     \
+    long long testn; \
+    cin >> testn;    \
+    while (testn--)
+#define all(x) x.begin(), x.end(),
+#define pb push_back
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define loopn(i, n) for (ll i = 0; i <= n; i++)
+#define loop1(i, n) for (ll i = 1; i < n; i++)
+#define loop1n(i, n) for (ll i = 1; i <= n; i++)
+
+using namespace std;
+
+void solve(){
+    int x;
+    cin >> x;
+    cout << x * x;
+}
+
+int main() {
+    IOS;
+    testcase{
+      solve();
+    }
+    return 0;
+}
